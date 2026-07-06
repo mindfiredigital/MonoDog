a70da1bef30c39787f0152d46098482244bad3ca
ansumanmindfire <ansumanp@mindfiresolutions.com>
2026-07-06 16:13:02 +0530

    fix(server): add custom root path for git services

--- packages/backend/src/serve-dev.ts
+++ packages/backend/src/serve-dev.ts
@@ -1,7 +1,10 @@
 import path from 'path';
 import { startServer } from './index';
 
-const rootPath = path.resolve(process.cwd(), '..', '..');
+const customRoot = process.env.MONODOG_TARGET_ROOT;
+const rootPath = customRoot
+  ? path.resolve(customRoot)
+  : path.resolve(process.cwd(), '..', '..');
 
 console.log(`Starting API server in DEV mode targeting ${rootPath}...`);
 startServer(rootPath, 4000, '0.0.0.0');
